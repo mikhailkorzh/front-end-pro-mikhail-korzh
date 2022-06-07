3f9a74ae2f4a9f583ae9328f8d309fb8ebcfddb1
mikhailkorzh <mikhail.korzh@gmail.com>
2022-06-07 15:50:05 +0300

    Solution for HomeWork#1 is added

---
+++ front-end-pro/homeWork_1/utils.js
@@ -0,0 +1,12 @@
+const num = 12345;
+const str = 'IT School \'Hillel\'';
+const bool = false;
+
+console.log('Hello World')
+console.log(`value: ${num}, type: ${typeof (num)}`);
+console.log(`value: ${str}, type: ${typeof (str)}`);
+console.log(`value: ${bool}, type: ${typeof (bool)}`);
+
+alert(`value: ${num}, type: ${typeof (num)}`);
+alert(`value: ${str}, type: ${typeof (str)}`);
+alert(`value: ${bool}, type: ${typeof (bool)}`);
