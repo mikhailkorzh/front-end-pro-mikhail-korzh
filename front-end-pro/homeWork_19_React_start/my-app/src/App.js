289cbc7864420a60438752f02bac492c7608d134
mikhailkorzh <mikhail.korzh@gmail.com>
2022-08-31 22:00:29 +0300

    React homeWork19

--- front-end-pro/homeWork_19_React_start/my-app/src/App.js
+++ front-end-pro/homeWork_19_React_start/my-app/src/App.js
@@ -1,36 +1,17 @@
 import './App.css';
 import React from 'react';
 
-const posts = [
-    {id: 1, title: 'Hello World!', content: 'Welcome to learning React!'},
-    {id: 2, title: 'Hello guys!', content: 'You can install React from npm.'},
-    {id: 3, title: 'Hello people!', content: 'Welcome to learning React!'}
-
-];
-
-function Post() {
-    const content = posts.map((post,index) =>
-        <li key={index}>
-        <div>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-        </div>
-        </li>
-    );
-    return (
-        <div>
-            <ul>
-                {content}
-            </ul>
-        </div>
-    );
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
 }
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Post/>
+        <div className='App'>
+            <header className='App-header'>
+                <Welcome name='Alice'/>
+                <Welcome name='Mike'/>
+                <Welcome name='Chris'/>
             </header>
         </div>
     );
